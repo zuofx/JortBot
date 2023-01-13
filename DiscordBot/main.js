3c5eeec8bff936429ca37aa846a5705b966e71c4
Huy T <huytruong.eq@gmail.com>
2023-01-12 20:03:29 -0500

    Update 3
    - Added flip a coin
    - Moved token data to a separate text file that is imported

--- DiscordBot/main.js
+++ DiscordBot/main.js
@@ -1,16 +1,10 @@
 // Default invite: https://discord.com/oauth2/authorize?client_id=1062794185779990609&scope=bot&permissions=277025601560
 
-// Constants: MAKE SURE TO UPDATE API TOKENS
-const TOKEN = ''
-const CLIENT_ID = ''
-const GUILD_ID = ''
-
-
+// INITIATIONS
 const Discord  = require('discord.js');
 const fs = require('fs')
 const { Client, GatewayIntentBits, REST, Routes, quote } = require('discord.js');
 const { request } = require('undici');
-
 
 // Functions:
 function randomNum(n) {
@@ -33,8 +27,28 @@ function textFile(a) {
 
 }
 
-// // // // // // // // // // // // // //
+function importTokens() {
+    let raw = fs.readFileSync('tokens.txt', 'utf8')
+    let holder = raw.split('\n')
 
+    for (i = 0; i < holder.length; i++) {
+        temp = holder[i]
+        temp = temp.split(' ')
+        holder[i] = temp[2].trim()
+    }
+
+    return holder
+
+}
+
+// // // // // // // // // // // // // //
+// Constants: MAKE SURE TO UPDATE API TOKENS
+allTokens = importTokens();
+const TOKEN = allTokens[0]
+const CLIENT_ID = allTokens[1]
+const GUILD_ID = allTokens[2]
+
+console.log(TOKEN, CLIENT_ID, GUILD_ID)
 
 const client = new Discord.Client({
   intents: [
@@ -114,7 +128,16 @@ client.on('interactionCreate', async interaction => {
         const quote = await output.body.json();
 
         interaction.editReply("*\""+ quote.content + "\"* - " + quote.author + "");
+    }
 
+    if (interaction.commandName === 'coinflip') {
+        let coin = randomNum(2)
+        if (coin == 0) {
+            await interaction.reply("The coin says: ***HEADS***")
+        }
+        if (coin == 1) {
+            await interaction.reply("The coin says: ***TAILS***")
+        }
     }
 
   });
